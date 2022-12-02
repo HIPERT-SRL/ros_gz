68093b377aa882d9beb2d76f789fe9cb63f5ff62
FisherTiger95 <massimiliano.bosi@hipert.it>
2022-12-02 05:30:09 +0000

    fix ros galactic build
    Signed-off-by: FisherTiger95 <massimiliano.bosi@hipert.it>

--- ros_gz_bridge/src/service_factory.hpp
+++ ros_gz_bridge/src/service_factory.hpp
@@ -18,6 +18,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <utility>
 
 #include <gz/transport/Node.hh>
@@ -28,70 +29,86 @@
 
 #include "service_factory_interface.hpp"
 
-namespace ros_gz_bridge
-{
+namespace ros_gz_bridge {
 
-template<typename RosResT>
-bool
-send_response_on_error(RosResT & ros_response);
+template <typename RosResT> bool send_response_on_error(RosResT &ros_response);
 
-template<typename RosServiceT, typename GzRequestT, typename GzReplyT>
-class ServiceFactory : public ServiceFactoryInterface
-{
+static std::unordered_map<std::string,
+                          std::pair<std::shared_ptr<gz::transport::Node>,
+                                    std::shared_ptr<rclcpp::ServiceBase>>>
+    sServices;
+
+template <typename RosServiceT, typename GzRequestT, typename GzReplyT>
+class ServiceFactory : public ServiceFactoryInterface {
 public:
-  ServiceFactory(
-    const std::string & ros_type_name, const std::string & gz_req_type_name,
-    const std::string & gz_rep_type_name)
-  : ros_type_name_(ros_type_name),
-    gz_req_type_name_(gz_req_type_name),
-    gz_rep_type_name_(gz_rep_type_name)
-  {}
+  ServiceFactory(const std::string &ros_type_name,
+                 const std::string &gz_req_type_name,
+                 const std::string &gz_rep_type_name)
+      : ros_type_name_(ros_type_name), gz_req_type_name_(gz_req_type_name),
+        gz_rep_type_name_(gz_rep_type_name) {}
 
   rclcpp::ServiceBase::SharedPtr
-  create_ros_service(
-    rclcpp::Node::SharedPtr ros_node,
-    std::shared_ptr<gz::transport::Node> gz_node,
-    const std::string & service_name) override
-  {
-    return ros_node->create_service<RosServiceT>(
-      service_name,
-      [gz_node = std::move(gz_node), service_name](
-        std::shared_ptr<rclcpp::Service<RosServiceT>> srv_handle,
-        std::shared_ptr<rmw_request_id_t> reqid,
-        std::shared_ptr<typename RosServiceT::Request> ros_req)
-      {
-        std::function<void(const GzReplyT &, bool)> callback;
-        callback = [
-          srv_handle = std::move(srv_handle),
-          reqid
-        ](
-          const GzReplyT & reply,
-          const bool result)
-        {
-          typename RosServiceT::Response ros_res;
-          if (!result) {
-            if (send_response_on_error(ros_res)) {
-              srv_handle->send_response(*reqid, ros_res);
-            }
-          }
-          convert_gz_to_ros(reply, ros_res);
-          srv_handle->send_response(*reqid, ros_res);
-        };
-        GzRequestT gz_req;
-        convert_ros_to_gz(*ros_req, gz_req);
-        gz_node->Request(
-          service_name,
-          gz_req,
-          callback);
-      });
+  create_ros_service(rclcpp::Node::SharedPtr ros_node,
+                     std::shared_ptr<gz::transport::Node> gz_node,
+                     const std::string &service_name) override {
+
+    std::function<void(
+        const std::shared_ptr<rmw_request_id_t> reqid,
+        const std::shared_ptr<typename RosServiceT::Request> ros_req,
+        std::shared_ptr<typename RosServiceT::Response> ros_res)>
+        callbackFunction =
+            std::bind(&ServiceFactory::service_callback, this,
+                      std::placeholders::_1, std::placeholders::_2,
+                      std::placeholders::_3, service_name);
+    std::shared_ptr<rclcpp::Service<RosServiceT>> srv_handle =
+        ros_node->create_service<RosServiceT>(service_name, callbackFunction);
+
+    std::cout << "Create " << service_name << std::endl;
+    sServices[service_name] = std::make_pair(std::move(gz_node), srv_handle);
+
+    std::cout << sServices[service_name].first.get() << " "
+              << sServices[service_name].second.get() << std::endl;
+
+    std::cout << "size: " << sServices.size() << std::endl;
+
+    return srv_handle;
   }
 
 private:
+  void service_callback(std::shared_ptr<rmw_request_id_t> reqid,
+                        std::shared_ptr<typename RosServiceT::Request> ros_req,
+                        std::shared_ptr<typename RosServiceT::Response> ros_res,
+                        const std::string &service_name) {
+
+    std::function<void(const GzReplyT &, bool)> callback =
+        [this, ros_res, service_name, reqid](const GzReplyT &reply,
+                                             const bool result) {
+          rclcpp::Service<RosServiceT> *srv_handle =
+              static_cast<rclcpp::Service<RosServiceT> *>(
+                  sServices[service_name].second.get());
+          if (!result) {
+            if (send_response_on_error(*ros_res)) {
+              srv_handle->send_response(*reqid, *ros_res);
+            }
+          }
+          convert_gz_to_ros(reply, *ros_res);
+          srv_handle->send_response(*reqid, *ros_res);
+        };
+
+    GzRequestT gz_req;
+    convert_ros_to_gz(*ros_req, gz_req);
+
+    sServices[service_name].first->Request(service_name, gz_req, callback);
+
+    reqid->sequence_number--;
+    std::cout << reqid->sequence_number << std::endl;
+  }
+
   std::string ros_type_name_;
   std::string gz_req_type_name_;
   std::string gz_rep_type_name_;
 };
 
-}  // namespace ros_gz_bridge
+} // namespace ros_gz_bridge
 
-#endif  // SERVICE_FACTORY_HPP_
+#endif // SERVICE_FACTORY_HPP_
